b239e7255bb284267b3c30dd61d07b9579437fd5
Jaeuk <enrtja5980@ajou.ac.kr>
2021-10-28 14:17:29 +0900

    커스텀 버튼 추가 및 아이콘 설치

---
+++ PyeonHee/client/Components/Buttons/JoinButton.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+
+export default class JoinButton extends Component{
+  static defaultProps = {
+    onPress: () => null,
+  }
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <TouchableOpacity style={styles.button} onPress={this.props.onPress}>
+          <Text style={styles.title}>회원가입</Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  button: {
+    flex: 1,
+    backgroundColor: '#F8F8FF',
+    width: 250,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 5,
+    marginBottom: 15,
+    borderRadius: 5,
+  },
+  title: {
+    fontSize: 15,
+    color: 'black',
+    fontWeight: 'bold',
+  },
+})
