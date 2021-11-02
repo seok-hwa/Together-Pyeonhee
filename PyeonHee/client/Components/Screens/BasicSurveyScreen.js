bf82d401e9c6ce011b5db02fe700e8c7c609a58e
Jaeuk <enrtja5980@ajou.ac.kr>
2021-11-02 23:01:01 +0900

    설문조사 전 기본 정보 입력 ui

--- PyeonHee/client/Components/Screens/BasicSurveyScreen.js
+++ PyeonHee/client/Components/Screens/BasicSurveyScreen.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-community/async-storage';
+import NextToMbtiButton from '../Buttons/NextToMbtiButton';
 import {
   SafeAreaView,
   ScrollView,
@@ -25,10 +26,104 @@ const BasicSurveyScreen = () => {
     });
   })
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Basic: {userID}</Text>
+    <View style={styles.appSize}>
+      <View style={styles.appLogoHeaderDiv}>
+        <Text style={styles.logoText}>설문조사전 기본 정보</Text>
+      </View>
+      <View style={styles.appBody}>
+        <View style={styles.appInnerBody}>
+          <View style={styles.innerTextAlign}>
+              <Text style={styles.questText}>월 수입</Text>
+          </View>
+          <View style={styles.textDiv}>
+            <TextInput 
+            style={styles.textInputDesign}
+            placeholder='숫자만 입력'
+            maxLength ={20}
+            />
+            <Text style={styles.wonText}> 원</Text>
+          </View>
+          <View style={styles.innerTextAlign}>
+              <Text style={styles.questText}>월 고정지출</Text>
+          </View>
+          <View style={styles.textDiv}>
+            <TextInput 
+            style={styles.textInputDesign}
+            placeholder='숫자만 입력'
+            maxLength ={20}
+            />
+            <Text style={styles.wonText}> 원</Text>
+          </View>
+          <View style={styles.innerTextAlign}>
+              <Text style={styles.questText}>월 저축액</Text>
+          </View>
+          <View style={styles.textDiv}>
+            <TextInput 
+            style={styles.textInputDesign}
+            placeholder='숫자만 입력'
+            maxLength ={20}
+            />
+            <Text style={styles.wonText}> 원</Text>
+          </View>            
+        </View>
+      </View>
+      <View style={styles.appFooter}>
+        <NextToMbtiButton />  
+      </View> 
     </View>
   )
 }
-
+const styles = StyleSheet.create({
+  appSize: {
+    flex: 1,
+  },
+  appLogoHeaderDiv: {
+    flex: 1,
+  },
+  logoText: {
+    marginTop: 50,
+    marginLeft: 20,
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'gray',
+  },
+  questText: {
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  wonText: {
+    fontSize: 15,
+    marginTop: 20,
+  },
+  appBody: {
+    flex: 4,
+    borderTopWidth: 1,
+    borderColor: 'gray',
+  },
+  appInnerBody: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  innerTextAlign: {
+    flexDirection: 'row',
+    width: 240,
+    marginTop: 30,
+  },
+  textDiv:{
+    flexDirection: 'row',
+  },
+  textInputDesign: {
+    marginTop: 10,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    height: 40,
+    width: 240,
+    borderRadius: 3,
+    backgroundColor: '#DCDCDC',
+  },
+  appFooter: {
+    flex: 2,
+    alignItems: 'center',
+  },
+});
 export default BasicSurveyScreen;
