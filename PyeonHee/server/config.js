a542ba986086d3feaf574de3ee21edca5182fd9a
Jaeuk <enrtja5980@ajou.ac.kr>
2021-11-12 13:16:12 +0900

    config 수정

--- PyeonHee/server/config.js
+++ PyeonHee/server/config.js
@@ -1,22 +1,22 @@
 const config = {
     dbServer: {
-        host: 'db-8dke6.cdb.ntruss.com',
-        port: 3306,
-        user: 'mysql_dba',
-        password: 'node1234!',
-        database: 'mysql-db'
+        host: '',
+        port: '',
+        user: '',
+        password: '',
+        database: ''
     },
     tunnelConfig: {
-        host: '106.10.56.88',
-        port: 8000,
-        username: 'root',
-        password: 'SWcapstone1105'
+        host: '',
+        port: '',
+        username: '',
+        password: ''
     },
     forwardConfig: {
-        srcHost: '127.0.0.1',
-        srcPort: 3306,
-        dstHost: 'db-8dke6.cdb.ntruss.com',
-        dstPort: 3306
+        srcHost: '',
+        srcPort: '',
+        dstHost: '',
+        dstPort: '',
     }
 };
 
