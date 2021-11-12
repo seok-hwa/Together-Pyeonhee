6689a3fc24fe45d3af1e3b1508d45d1d1af605f1
Jeong Seock Hwa <71437951+SeockHwa@users.noreply.github.com>
2021-11-12 10:35:50 +0900

    Update config.js

--- PyeonHee/server/config.js
+++ PyeonHee/server/config.js
@@ -1,21 +1,21 @@
 const config = {
     dbServer: {
-        host: 'db-8dke6.cdb.ntruss.com',
-        port: 3306,
-        user: 'mysql_dba',
-        password: 'node1234!',
-        database: 'mysql-db'
+        host: '',
+        port: ,
+        user: '',
+        password: '',
+        database: ''
     },
     tunnelConfig: {
-        host: '106.10.56.88',
-        port: 8000,
-        username: 'root',
-        password: 'SWcapstone1105'
+        host: '',
+        port: ,
+        username: '',
+        password: ''
     },
     forwardConfig: {
-        srcHost: '127.0.0.1',
-        srcPort: 3306,
-        dstHost: 'db-8dke6.cdb.ntruss.com',
+        srcHost: '',
+        srcPort: ,
+        dstHost: '',
         dstPort: 3306
     }
 };
