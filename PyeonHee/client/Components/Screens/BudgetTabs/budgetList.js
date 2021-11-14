4544642537d415f2c2de79a644155697216d03f4
soy <thdl9893@ajou.ac.kr>
2021-11-14 17:55:16 +0900

    추천예산목록 체크박스 추가

--- PyeonHee/client/Components/Screens/BudgetTabs/budgetList.js
+++ PyeonHee/client/Components/Screens/BudgetTabs/budgetList.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-community/async-storage';
 import { StyleSheet, Text, View, ScrollView} from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
+
 import BudgetItem from '../BudgetItem';
 import config from '../../../config';
 
@@ -8,6 +10,8 @@ const url = config.url;
 const BudgetList = ({navigation}) => {
     const [userID, setUserID] = useState('');
     const [otherBudgetData, setOtherBudgetData] = useState([]);
+    const [recommendedBudgetData, setRecommendedBudgetData] = useState([]);
+    const [check, setCheck] = useState(false);
 
     useEffect(()=>{
         let tempID;
@@ -30,6 +34,14 @@ const BudgetList = ({navigation}) => {
                 console.log(responseJson);
                 setOtherBudgetData(responseJson);
             })  
+
+            fetch(`${url}/viewBudgetPlan?userID=${tempID}`)   //get
+            .then((response)=>response.json())
+            .then((responseJson)=>{
+                console.log('response data');
+                console.log(responseJson);
+                setRecommendedBudgetData(responseJson);
+            })
             
         })
     }, [])   
@@ -39,14 +51,29 @@ const BudgetList = ({navigation}) => {
         <View style={styles.appSize}>
             {/* <Text>본인 예산 계획서</Text> */}
             <ScrollView>
+                <View style={styles.wrapper}>
+                    <CheckBox value={check} onChange={() => setCheck(!check)} />
+                    <Text style={styles.text}>
+                        나와 유사한 계획서 찾기
+                    </Text>
+                </View>
                 <View>
-                    {
+                    {check === false && 
                         otherBudgetData.map(item => {
                         return <BudgetItem userAge={item.user_age} key={item.planning_number} budgetPlanningID={item.planning_number} navigation={navigation} userIncome={item.user_income} 
                         userFixedExpense={item.monthly_rent+item.insurance_expense+item.transportation_expense+item.communication_expense+item.education_expense} 
                         userVariableExpense={item.leisure_expense+ item.shopping_expense+ item.medical_expense+ item.event_expense + item.etc_expense} 
                         />;
                     })}
+                    {check === true && 
+                    
+                        recommendedBudgetData.map(item => {
+                        return <BudgetItem userAge={item.user_age} key={item.planning_number} budgetPlanningID={item.planning_number} navigation={navigation} userIncome={item.user_income} 
+                        userFixedExpense={item.monthly_rent+item.insurance_expense+item.transportation_expense+item.communication_expense+item.education_expense} 
+                        userVariableExpense={item.leisure_expense+ item.shopping_expense+ item.medical_expense+ item.event_expense + item.etc_expense} 
+                        />;
+                    })}
+                
                 </View>
             </ScrollView>
         </View>
@@ -74,6 +101,16 @@ const styles = StyleSheet.create({
     },
     appTitle: {
         fontSize: 15,
+    },
+    wrapper: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignContent: 'center',
+        paddingVertical: 5,
+    },
+    text: {
+        lineHeight: 30,
+        marginLeft: 10,
     },
 })
 export default BudgetList;
