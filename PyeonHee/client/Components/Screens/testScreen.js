26ced280a6fbcab37bec7eb0249eef4adfe2c17d
Jaeuk <enrtja5980@ajou.ac.kr>
2021-11-16 15:46:20 +0900

    오픈뱅킹 테스트 중

--- PyeonHee/client/Components/Screens/testScreen.js
+++ PyeonHee/client/Components/Screens/testScreen.js
@@ -3,60 +3,43 @@ import AsyncStorage from '@react-native-community/async-storage';
 import config from '../../config';
 import { WebView } from 'react-native-webview';
 import {
-    BackHandler,
+    SafeAreaView,
 } from 'react-native';
 import Loading from './Loading';
 
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  Image,
-  Button,
-  TouchableOpacity,
-  TextInput,
-} from 'react-native';
 const url = config.url;
+const openBankingURL = encodeURI(config.openBankingURL);
 
 class testScreen extends Component {
-    webView = {
-      canGoBack: false,
-      ref: null,
-    }
   
-    onAndroidBackPress = () => {
-      if (this.webView.canGoBack && this.webView.ref) {
-        this.webView.ref.goBack();
-        return true;
-      }
-      return false;
-    }
-  
-    componentDidMount() {
-      if (Platform.OS === 'android') {
-        BackHandler.addEventListener('hardwareBackPress', this.onAndroidBackPress);
-      }
-    }
-  
-    componentWillUnmount() {
-      if (Platform.OS === 'android') {
-        BackHandler.removeEventListener('hardwareBackPress');
-      }
-    }
-  
-    render() {
-      return (
-        <WebView
-          source={{ uri: "https://testapi.openbanking.or.kr/oauth/2.0/authorize?response_type=code&client_id=f2126f98-9b2b-4c72-8978-42f5e8bd836c&redirect_uri=https://localhost:8000/Together&scope=login inquiry transfer&state=12345678901234567890123456789012&auth_type=0" }}
-          ref={(webView) => { this.webView.ref = webView; }}
-          onNavigationStateChange={(navState) => { this.webView.canGoBack = navState.canGoBack; }}
-        />
-      );
-    }
+  render() {
+    function authProgress(data) {
+        // access code는 url에 붙어 장황하게 날아온다.
+        // substringd으로 url에서 code=뒤를 substring하면 된다.
+        const exp = "code=";
+        var condition = data.indexOf(exp);
+        if (condition != -1) {
+          var request_code = data.substring(condition + exp.length);
+          // console.log("access code :: " + request_code);
+          // 토큰값 받기
+          requestToken(request_code);
+        }
+    }  
+    return (
+      <WebView
+        source={{ uri: openBankingURL }}
+        startInLoadingState={true}
+        renderLoading={() => <Loading />}
+        originWhitelist={['*']}
+        scalesPageToFit={false}
+        onError={(syntheticEvent) => {
+          const { nativeEvent } = syntheticEvent;
+          console.warn('WebView error: ', nativeEvent);
+        }}
+        onMessage={(event) => { authProgress(event.nativeEvent["url"]); }}
+      />
+    );
   }
-  
-  export default testScreen
+}
+
+export default testScreen
