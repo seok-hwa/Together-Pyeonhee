6a365daf73c12ad0670b54f9f28f5f78ba3ea15d
Jaeuk <enrtja5980@ajou.ac.kr>
2021-11-19 18:06:57 +0900

    계좌 등록 테스트 완료

--- PyeonHee/server/app.js
+++ PyeonHee/server/app.js
@@ -619,7 +619,7 @@ const SSHConnection = new Promise((resolve, reject) => {
                 여기서 토큰을 db에 저장해서 나중에 저장 api에서 사용자 id랑 토큰 비교 후 다시 저장하면 될 듯
                 
                 */
-                res.send("<script>alert('조금 기다려 주세요');</script><h1 style=\"text-align: center; vertical-align: center;\">인증을 진행중입니다.</h1> <h4 style=\"text-align: center; vertical-align: center;\">뒤로가기를 눌러 확인해주세요.</h4>");
+                res.send("<h1 style=\"text-align: center; vertical-align: center;\">인증을 진행중입니다.</h1> <h4 style=\"text-align: center; vertical-align: center;\">뒤로가기를 눌러 확인해주세요.</h4>");
                 
                 
                 //프론트에서 발급 받고 여기로 자동 redirect되므로 프론트에서 진행
