46cbcbccfc01e693e35e1c0907b6793f40cb6d62
seockas <shjeong008@ajou.ac.kr>
2021-11-21 02:20:20 +0900

    add new data

--- PyeonHee/server/query.sql
+++ PyeonHee/server/query.sql
@@ -369,3 +369,197 @@ create table real_expense
     primary key (user_id, fintech_use_num, tran_date, tran_time),
     foreign key (user_id, fintech_use_num) references bank_account(user_id, fintech_use_num)
 );
+
+-- 신규 데이터 생성 (2021/11/21)
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user1', '1234', 'user1', 'ICSE', '25', 'GOLD', 1500000, 'profession');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user2', '1234', 'user2', 'ICSE', '27', 'SILVER', 2500000, 'teacher');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user3', '1234', 'user3', 'ICSE', '23', 'BROZE', 3500000, 'office');
+
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user4', '1234', 'user4', 'ICSM', '21', 'GOLD', 1000000'student');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user5', '1234', 'user5', 'ICSM', '24', 'SILVER', 1500000, 'office');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user6', '1234', 'user6', 'ICSM', '25', 'PLATINUM', 3500000, 'selfEmployment');
+
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user7', '1234', 'user7', 'ICOE', '26', 'PLATINUM', 2500000, 'executive');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user8', '1234', 'user8', 'ICOE', '29', 'GOLD', 4500000, 'service');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user9', '1234', 'user9', 'ICOE', '25', 'SILVER', 1500000, 'housewife');
+
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user10', '1234', 'user0', 'ICOM', '20', 'GOLD', 1500000, 'student');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user11', '1234', 'user11', 'ICOM', '21', 'SILVER', 2500000, 'service');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user12', '1234', 'user12', 'ICOM', '27', 'GOLD', 4500000, 'profession');
+
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user13', '1234', 'user13', 'IHSE', '28', 'SILVER', 1500000, 'sales');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user14', '1234', 'user14', 'IHSE', '24', 'GOLD', 3500000, 'agriculture');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user15', '1234', 'user15', 'IHSE', '21', 'BROZE', 1500000, 'student');
+
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user16', '1234', 'user16', 'IHSM', '23', 'SILVER', 3500000, 'office');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user17', '1234', 'user17', 'IHSM', '24', 'BROZE', 3500000, 'executive');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user18', '1234', 'user18', 'IHSM', '25', 'GOLD', 2500000, 'sales');
+
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user19', '1234', 'user19', 'IHOE', '27', 'BROZE', 3500000, 'teacher');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user20', '1234', 'user20', 'IHOE', '25', 'GOLD', 1500000, 'selfEmployment');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user21', '1234', 'user21', 'IHOE', '28', 'PLATINUM', 3500000, 'office');
+
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user22', '1234', 'user22', 'IHOM', '23', 'GOLD', 1500000, 'student');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user23', '1234', 'user23', 'IHOM', '22', 'PLATINUM', 1500000, 'housewife');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user24', '1234', 'user24', 'IHOM', '26', 'SILVER', 3500000, 'service');
+
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user25', '1234', 'user25', 'PCSE', '25', 'GOLD', 5500000, 'profession');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user26', '1234', 'user26', 'PCSE', '25', 'SILVER', 1500000, 'profession');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user27', '1234', 'user27', 'PCSE', '25', 'DIAMOND', 1500000, 'profession');
+
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user28', '1234', 'user28', 'PCSM', '27', 'BRONZE', 2500000, 'agriculture');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user29', '1234', 'user29', 'PCSM', '25', 'BRONZE', 1500000, 'agriculture');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user30', '1234', 'user30', 'PCSM', '25', 'GOLD', 1500000, 'inoccupation');
+
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user31', '1234', 'user31', 'PCOE', '25', 'GOLD', 1500000, 'selfEmployment');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user32', '1234', 'user32', 'PCOE', '27', 'SILVER', 3500000, 'profession');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user33', '1234', 'user33', 'PCOE', '25', 'GOLD', 1500000, 'housewife');
+
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user34', '1234', 'user34', 'PCOM', '25', 'BRONZE', 1500000, 'housewife');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user35', '1234', 'user35', 'PCOM', '29', 'SILVER', 4500000, 'profession');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user36', '1234', 'user36', 'PCOM', '25', 'BRONZE', 1500000, 'housewife');
+
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user37', '1234', 'user37', 'PHSE', '27', 'GOLD', 5500000, 'selfEmployment');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user38', '1234', 'user38', 'PHSE', '25', 'SILVER', 1500000, 'craftsman');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user39', '1234', 'user39', 'PHSE', '25', 'GOLD', 1500000, 'profession');
+
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user40', '1234', 'user40', 'PHSM', '27', 'PLATINUM', 5500000, 'agriculture');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user41', '1234', 'user41', 'PHSM', '27', 'PLATINUM', 1500000, 'student');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user42', '1234', 'user42', 'PHSM', '25', 'SILVER', 1500000, 'retirement');
+
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user43', '1234', 'user43', 'PHOE', '25', 'GOLD', 1500000, 'profession');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user44', '1234', 'user44', 'PHOE', '25', 'SILVER', 1500000, 'sales');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user45', '1234', 'user45', 'PHOE', '25', 'GOLD', 1500000, 'craftsman');
+
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user46', '1234', 'user46', 'PHOM', '25', 'PLATINUM', 2500000, 'profession');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user47', '1234', 'user47', 'PHOM', '25', 'SILVER', 2500000, 'production');
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user48', '1234', 'user48', 'PHOM', '25', 'SILVER', 2500000, 'retirement');
+
+-- 추천목록
+-- 즉흥적이고 소비형이며 본인 경험적
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user49', '1234', 'user49', 'ICSE', '25', 'DIAMOND', 3500000, 'executive');
+insert into BudgetPlanning(user_id, user_mbti, user_age, like_number, user_income, user_savings, 
+monthly_rent, insurance_expense, transportation_expense, communication_expense, leisure_expense,
+shopping_expense, education_expense, medical_expense, event_expense, etc_expense, subscribe_expense)
+values ('user49', 'ICSE', '25', 496, 3500000, 500000, 600000, 100000, 150000, 100000, 500000, 500000, 0, 0, 0, 100000, 100000);
+
+-- 즉흥 절약 본인 경험
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user50', '1234', 'user50', 'IHSE', '27', 'DIAMOND', 2500000, 'office');
+insert into BudgetPlanning(user_id, user_mbti, user_age, like_number, user_income, user_savings, 
+monthly_rent, insurance_expense, transportation_expense, communication_expense, leisure_expense,
+shopping_expense, education_expense, medical_expense, event_expense, etc_expense, subscribe_expense)
+values ('user50', 'IHSE', '27', 511, 2900000, 130000, 0, 50000, 100000, 70000, 300000, 200000, 0, 0, 50000, 0, 50000);
+
+-- 즉흥 소비 타인 물질
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user51', '1234', 'user51', 'ICOM', '23', 'DIAMOND', 2500000, 'service');
+insert into BudgetPlanning(user_id, user_mbti, user_age, like_number, user_income, user_savings, 
+monthly_rent, insurance_expense, transportation_expense, communication_expense, leisure_expense,
+shopping_expense, education_expense, medical_expense, event_expense, etc_expense, subscribe_expense)
+values ('user51', 'ICOE', '27', 647, 2700000, 700000, 0, 100000, 100000, 0, 300000, 500000, 0, 0, 200000, 0, 50000);
+
+-- 즉흥 절약 타인 경험
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user52', '1234', 'user52', 'IHOE', '21', 'DIAMOND', 1500000, 'student');
+insert into BudgetPlanning(user_id, user_mbti, user_age, like_number, user_income, user_savings, 
+monthly_rent, insurance_expense, transportation_expense, communication_expense, leisure_expense,
+shopping_expense, education_expense, medical_expense, event_expense, etc_expense, subscribe_expense)
+values ('user52', 'IHOE', '21', 623, 1300000, 200000, 0, 0, 70000, 0, 300000, 0, 0, 0, 150000, 0, 50000);
+
+-- 즉흥 절약 타인 물질
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user53', '1234', 'user53', 'IHOM', '29', 'PLATINUM', 4500000, 'profession');
+insert into BudgetPlanning(user_id, user_mbti, user_age, like_number, user_income, user_savings, 
+monthly_rent, insurance_expense, transportation_expense, communication_expense, leisure_expense,
+shopping_expense, education_expense, medical_expense, event_expense, etc_expense, subscribe_expense)
+values ('user53', 'IHOM', '29', 372, 4800000, 2000000, 500000, 100000, 70000, 100000, 300000, 500000, 0, 0, 300000, 0, 100000);
+
+-- 계획 소비 본인 경험 
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user54', '1234', 'user54', 'PCSE', '25', 'DIAMOND', 3500000, 'executive');
+insert into BudgetPlanning(user_id, user_mbti, user_age, like_number, user_income, user_savings, 
+monthly_rent, insurance_expense, transportation_expense, communication_expense, leisure_expense,
+shopping_expense, education_expense, medical_expense, event_expense, etc_expense, subscribe_expense)
+values ('user54', 'PCSE', '25', 455, 3400000, 500000, 600000, 100000, 150000, 100000, 500000, 500000, 300000, 0, 0, 100000, 100000);
+
+-- 계획 절약 타인 경험
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user55', '1234', 'user55', 'PHOE', '24', 'DIAMOND', 3500000, 'craftsman');
+insert into BudgetPlanning(user_id, user_mbti, user_age, like_number, user_income, user_savings, 
+monthly_rent, insurance_expense, transportation_expense, communication_expense, leisure_expense,
+shopping_expense, education_expense, medical_expense, event_expense, etc_expense, subscribe_expense)
+values ('user55', 'PHOE', '24', 512, 3200000, 1400000, 600000, 50000, 100000, 50000, 200000, 100000, 0, 0, 150000, 0, 50000);
+
+-- 계획 절약 본인 경험
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user56', '1234', 'user56', 'PHSE', '23', 'DIAMOND', 2500000, 'sales');
+insert into BudgetPlanning(user_id, user_mbti, user_age, like_number, user_income, user_savings, 
+monthly_rent, insurance_expense, transportation_expense, communication_expense, leisure_expense,
+shopping_expense, education_expense, medical_expense, event_expense, etc_expense, subscribe_expense)
+values ('user56', 'PHSE', '23', 572, 2400000, 1000000, 400000, 50000, 100000, 50000, 200000, 0, 0, 0, 50000, 0, 50000);
+
+-- 계획 소비 타인 물질
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user57', '1234', 'user57', 'PCOM', '21', 'DIAMOND', 1500000, 'student');
+insert into BudgetPlanning(user_id, user_mbti, user_age, like_number, user_income, user_savings, 
+monthly_rent, insurance_expense, transportation_expense, communication_expense, leisure_expense,
+shopping_expense, education_expense, medical_expense, event_expense, etc_expense, subscribe_expense)
+values ('user57', 'IHOE', '21', 482, 1200000, 200000, 0, 100000, 0, 100000, 300000, 0, 0, 0, 100000, 0, 0);
+
+-- 계획 절약 본잍 물질
+insert into user(user_id, password, name, mbti, age, tier, income, job)
+values ('user58', '1234', 'user58', 'PHSM', '29', 'PLATINUM', 4500000, 'profession');
+insert into BudgetPlanning(user_id, user_mbti, user_age, like_number, user_income, user_savings, 
+monthly_rent, insurance_expense, transportation_expense, communication_expense, leisure_expense,
+shopping_expense, education_expense, medical_expense, event_expense, etc_expense, subscribe_expense)
+values ('user58', 'PHSM', '29', 452, 4500000, 2200000, 500000, 100000, 100000, 100000, 300000, 500000, 0, 0, 100000, 0, 100000);
