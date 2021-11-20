f7979d02036ae4f9f2ebf480f10905b5af1d84d0
soy <thdl9893@ajou.ac.kr>
2021-11-20 23:33:12 +0900

    캘린더 데이터 이동 확인

--- PyeonHee/client/Components/Screens/HomeTabs/calendarComponent/TransactionList.js
+++ PyeonHee/client/Components/Screens/HomeTabs/calendarComponent/TransactionList.js
@@ -9,30 +9,7 @@ const url = config.url;
 const TransactionList = (props) => {
     const [userID, setUserID] = useState('');
     const [todayTransaction, setTodayTransaction] = useState([]);
-    // const [loading, setLoading] = useState(false);
-
-    // let todayTransaction = [
-    //     {
-    //         name: '아침',
-    //         money: 1000,
-    //         id: 1,
-    //     },
-    //     {
-    //         name: '점심',
-    //         money: 5000,
-    //         id: 2,
-    //     },
-    //     {
-    //         name: '저녁',
-    //         money: 10000,
-    //         id: 3,
-    //     },
-    //     {
-    //         name: '출금',
-    //         money: -10000,
-    //         id: 3,
-    //     },
-    // ]
+    const [loading, setLoading] = useState(false);
 
     useEffect(()=>{
         let tempID;
@@ -45,28 +22,21 @@ const TransactionList = (props) => {
         })
         .then(()=>{
             console.log(tempID);
-            // console.log(`${url}/calendar/click?userID=${tempID}?today=${}`)/
-            // let tempDay = props.pressedYear+props.pressedMonth+props.pressedDate;/
-            let temp = 20211120;
-            console.log(`${url}/calendar/click?userID=${tempID}&today=${temp}`);
-            // console.log(tempDay);
-            // console.log('tempDay');
-
-            
-            fetch(`${url}/calendar/click?userID=${tempID}&today=${temp}`)   //get 오늘 날짜도 보내주기
+      
+            let tempDay = props.pressedDay;
+            console.log(`${url}/calendar/click?userID=${tempID}&today=${tempDay}`);
+    
+            fetch(`${url}/calendar/click?userID=${tempID}&today=${tempDay}`)   //get 오늘 날짜도 보내주기
             .then((response)=>response.json())
             .then((responseJson)=>{
-                console.log('response data');
+                console.log('오늘의 거래 내역');
                 console.log(responseJson);
 
                 setTodayTransaction(responseJson);
-                // setLoading(true);
-            })  
-            
-
-
+                setLoading(true);
+            })
         })
-    }, [])
+    })  //-> 랜더링 문제 해결 필요
 
     return (
         <View>
@@ -75,11 +45,11 @@ const TransactionList = (props) => {
             </View>
 
             <View>
+                
                 {todayTransaction.length === 0 ?
                 <Text>거래 내역이 없습니다.</Text> :
-                todayTransaction.map(item => {
-                    return <TransactionItem key={item.id} name={item.name} money={item.money}/>;
-                })}
+                <Text>거래 내역이 있습니다.</Text>}
+                
             </View>
 
         </View>
