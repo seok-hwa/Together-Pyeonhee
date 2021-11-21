d45ee1654b1ffbccf9d25acd920d25e78e75db1e
Jaeuk <enrtja5980@ajou.ac.kr>
2021-11-21 18:43:50 +0900

    거래 내역 오류 수정

--- PyeonHee/client/Components/Screens/HomeTabs/TransactionalInfoScreen.js
+++ PyeonHee/client/Components/Screens/HomeTabs/TransactionalInfoScreen.js
@@ -44,7 +44,7 @@ const TransactionScreen = ({navigation}) => {
             })
             .then((response)=>response.json())
             .then((responseJson)=>{
-                console.log('response data');
+                console.log('최근 거래 내역');
                 console.log(responseJson);
 
                 setTranLatestList(responseJson);
@@ -63,7 +63,7 @@ const TransactionScreen = ({navigation}) => {
                 })
                 .then((response)=>response.json())
                 .then((responseJson)=>{
-                    console.log('response data');
+                    console.log('거래 내역');
                     console.log(responseJson);
     
                     setTranList(responseJson);
@@ -164,7 +164,7 @@ const TransactionScreen = ({navigation}) => {
                     <View style={styles.tranCate}><Text style={styles.graphFont}>종류</Text></View>
                 </View>
                 <ScrollView style={{flex: 1,}}>
-                    {tranList.map(item => {
+                    {tranList.map((item, index )=> {
                         return <TransactionItem key={index} bankName={item.bank_name} branchName={item.branch_name} tranDate={item.tran_date} 
                         tranPrice={item.tran_amt} tranTime={item.tran_time} tranCate={item.tran_type} tranID={item.tranID} navigation={navigation}
                         inoutType={item.inout_type} fintech={item.fintech_use_num} organizationName={item.print_content}
