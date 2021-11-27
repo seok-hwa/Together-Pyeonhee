54e0004bc165081711b80a2c73106b57aeb1bb33
Jaeuk <enrtja5980@ajou.ac.kr>
2021-11-27 15:59:27 +0900

    관리자 웹페이지 공지사항, 고객센터 ui

--- PyeonHee/admin/src/pages/Notification.js
+++ PyeonHee/admin/src/pages/Notification.js
@@ -1,15 +1,44 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import '../App.css';
+import {Paper, Table, TableHead, TableBody, TableRow, TableCell } from "@material-ui/core";
+import {withStyles} from '@material-ui/core/styles';
 
-function Notification() {
+const styles = theme => ({
+  root: {
+    width: '100%',
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  },
+
+})
+function Notification(props) {
+  const {classes} = props;
+
   return (
     <div className="NotificationDiv">
       <p className="NotificationTitleText">공지사항</p>
+      <div className="NotificationWriteButtonDiv">
+          <button className="NotificationWriteButton" type='button'>공지글 게시</button>
+      </div>
       <div className="NotificationBodyDiv">
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>분류</TableCell>
+                <TableCell>제목</TableCell>
+                <TableCell>날짜</TableCell>
+              </TableRow>
+            </TableHead>
+          </Table>
+        </Paper>
       </div>
     </div>
   );
 }
 
-export default Notification;
+export default withStyles(styles)(Notification);
--- PyeonHee/admin/src/pages/ServiceCenter.js
+++ PyeonHee/admin/src/pages/ServiceCenter.js
@@ -1,15 +1,40 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import '../App.css';
+import {Paper, Table, TableHead, TableBody, TableRow, TableCell } from "@material-ui/core";
+import {withStyles} from '@material-ui/core/styles';
+const styles = theme => ({
+  root: {
+    width: '100%',
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  },
 
-function ServiceCenter() {
+})
+function ServiceCenter(props) {
+  const {classes} = props;
   return (
     <div className="ServiceDiv">
       <p className="ServiceTitleText">고객센터</p>
       <div className="ServiceBodyDiv">
+      <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>분류</TableCell>
+                <TableCell>제목</TableCell>
+                <TableCell>작성자ID</TableCell>
+                <TableCell>날짜</TableCell>
+              </TableRow>
+            </TableHead>
+          </Table>
+        </Paper>
       </div>
     </div>
   );
 }
 
-export default ServiceCenter;
+export default withStyles(styles)(ServiceCenter);
