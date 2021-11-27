9584673c9b893c26f25b2e3890e3a1646678f26c
Jaeuk <enrtja5980@ajou.ac.kr>
2021-11-27 21:20:40 +0900

    공지사항 글 확인 구현

--- PyeonHee/admin/src/pages/NotificationBoard.js
+++ PyeonHee/admin/src/pages/NotificationBoard.js
@@ -3,13 +3,51 @@ import axios from 'axios';
 import '../App.css';
 
 function NotificationBoard({ match }) {
-return (
-  <div className="NotificationDiv">
-    <p className="NotificationTitleText">공지사항 작성</p>
-    <div className="NotificationBodyDiv">
-
+  const [boardTitle, setBoardTitle] = useState('안녕하세요');
+  const [boardContent, setBoardContent] = useState('모두들 반갑습니다.');
+  const [boardDate, setBoardDate] = useState('2021-11-27');
+  const [boardCate, setBoardCate] = useState('티어');
+  /*
+  useEffect(() => {
+    axios({
+      method:"POST",
+      url: `/notificationBoard`,
+      data:{
+          boardID: match.params.boardID,
+      }
+    })
+    .then((res)=>{
+        console.log(res.data);
+        setBoardTitle(res.data.boardTitle);
+        setBoardContent(res.data.boardContent);
+        setBoardDate(res.data.boardDate);
+        setBoardCate(res.data.boardCate);
+    }).catch(error=>{
+        console.log(error);
+    });
+  },[])*/
+  return (
+    <div className="NotificationBoardDiv">
+      <p className="NotificationTitleText">공지사항 확인</p>
+      <div className="NotificationBodyDiv">
+        <div className="NotificationBoardTitleDiv">
+          <p className="NotificationBoardTitleFont">제목:&nbsp;</p>
+          <p className="NotificationBoardTitleFont">{boardTitle}</p>
+        </div>
+        <div className="NotificationBoardDateDiv">
+          <p className="NotificationBoardDateFont">작성일: {boardDate}</p>
+        </div>
+        <div className="NotificationBoardOuterContentDiv">
+          <div className="NotificationBoardInnerContentDiv">
+            <p className="NotificationBoardContentFont">{boardContent}</p>
+          </div>
+        </div>
+        <div className="NotificationBoardButtonDiv">
+          <button className="NotificationUpdateButton" type='button' >수정</button>
+          <button className="NotificationDeleteButton" type='button' >삭제</button>
+        </div>
+      </div>
     </div>
-  </div>
   );
 }
 
