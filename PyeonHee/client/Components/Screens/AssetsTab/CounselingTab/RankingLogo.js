55bb99a1e95cea20c45dbebb9e51d6def1dc07c6
soy <thdl9893@ajou.ac.kr>
2021-12-03 03:34:29 +0900

    상담목록 UI 수정

--- PyeonHee/client/Components/Screens/AssetsTab/CounselingTab/RankingLogo.js
+++ PyeonHee/client/Components/Screens/AssetsTab/CounselingTab/RankingLogo.js
@@ -5,31 +5,51 @@ const RankingLogo = (props) => {
     const ranking = props.rank;
     if(ranking === 1){
         return(
-            <Image source={require('../../assets/ranking/first.png')} style={styles.accountImage}/>
+            <Image source={require('../../assets/ranking/first.png')} style={styles.rankingImage}/>
         )
     }else if(ranking === 2){
         return(
-            <Image source={require('../../assets/ranking/second.png')} style={styles.accountImage}/>
+            <Image source={require('../../assets/ranking/second.png')} style={styles.rankingImage}/>
         )
     }else if(ranking === 3){
         return(
-            <Image source={require('../../assets/ranking/third.png')} style={styles.accountImage}/>
+            <Image source={require('../../assets/ranking/third.png')} style={styles.rankingImage}/>
         )
     }
     else{
         return(
-            <View style={styles.accountImage} />
+            <View style={styles.rankContainer} >
+                <Text style={{fontSize: 18, fontWeight: 'bold'}}>
+                    {ranking}
+                </Text>
+            </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    accountImage: {
-        height: 25,
-        width: 25,
+    rankingLogoContainer: {
+        width: 30,
+        height: 30,
+        margin: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: 'gray',
+      },
+    rankingImage: {
+        height: 30,
+        width: 30,
         // borderWidth: 1,
         // borderRadius: 2,
     },
+    rankContainer: {
+        height: 25,
+        width: 25,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 });
 
 export default RankingLogo;
