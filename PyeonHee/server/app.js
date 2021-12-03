661d077fc9e22e8608bd70ecc635f672c3cda4cd
SeockHwa <shjeong008@ajou.ac.kr>
2021-12-04 01:00:52 +0900

    Update app.js

--- PyeonHee/server/app.js
+++ PyeonHee/server/app.js
@@ -163,24 +163,24 @@ const SSHConnection = new Promise((resolve, reject) => {
             });
 
             // 30분마다 일일소비량 업데이트
-            schedule.scheduleJob('0 */30 * * * *', async () => {
-                db.query(`SELECT sum(tran_amt) as spend_money FROM real_expense WHERE DAY(now()) = SUBSTR(tran_date, 7,2) AND user_id = ?`,[global_id], function(error1, result1){
-                    if(error1) throw error1;
-                    else{
-                        if(result1.spend_money == null)
-                            console.log('소비한 내역이 없습니다.');
-                        else{
-                            console.log(result1);
-                            daily_spent_money = result1[0].spend_money
-                            db.query(`UPDATE daily_data SET  daily_spent_money= ? WHERE user_id = ?`,[daily_spent_money, global_id], function(error2, result2){
-                                   if(error2) throw error2;
-                                console.log(result2);
-                            })
-                        }
-                    }
+            // schedule.scheduleJob('*/30 * * * * *', async () => {
+            //     db.query(`SELECT sum(tran_amt) as spend_money FROM real_expense WHERE DAY(now()) = SUBSTR(tran_date, 7,2) AND user_id = ?`,[global_id], function(error1, result1){
+            //         if(error1) throw error1;
+            //         else{
+            //             if(result1.spend_money == null)
+            //                 console.log('소비한 내역이 없습니다.');
+            //             else{
+            //                 console.log(result1);
+            //                 daily_spent_money = result1[0].spend_money
+            //                 db.query(`UPDATE daily_data SET  daily_spent_money= ? WHERE user_id = ?`,[daily_spent_money, global_id], function(error2, result2){
+            //                        if(error2) throw error2;
+            //                     console.log(result2);
+            //                 })
+            //             }
+            //         }
                     
-                })
-            });
+            //     })
+            // });
 
             
             //일일권장 소비금액 (잔액 푸시알림)
