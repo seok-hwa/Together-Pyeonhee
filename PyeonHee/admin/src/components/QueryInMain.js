0eeb46c4666088719b4e538d8f121882a48cb6ea
Jaeuk <enrtja5980@ajou.ac.kr>
2021-12-05 01:24:41 +0900

    관리자 페이지 메인 화면 테스트 완료

--- PyeonHee/admin/src/components/QueryInMain.js
+++ PyeonHee/admin/src/components/QueryInMain.js
@@ -5,9 +5,9 @@ function QueryInMain(props) {
     return (
       <TableRow>
           <TableCell>{props.category}</TableCell>
-          <TableCell><a href={`/queryBoard/${props.id}`}>{props.title}</a></TableCell>
+          <TableCell><a href={`/queryBoard/${props.id}`} className="box">{props.title}</a></TableCell>
           <TableCell>{props.userID}</TableCell>
-          <TableCell>{props.date}</TableCell>
+          <TableCell>{props.date.substring(0,10)}</TableCell>
       </TableRow>
     );
   }
--- PyeonHee/admin/src/pages/Main.js
+++ PyeonHee/admin/src/pages/Main.js
@@ -31,7 +31,7 @@ function Main(props) {
       url: '/notificationInMain',
     })
     .then((res)=>{
-      console.log(res.data);
+      console.log('공지사항: ',res.data);
       setNotifications(res.data);
     })
     .then(()=>{
@@ -40,7 +40,7 @@ function Main(props) {
         url: '/queryInMain',
       })
       .then((res)=>{
-        console.log(res.data);
+        console.log('고객센터: ',res.data);
         setQueries(res.data);
       })
       .then(()=>{
@@ -58,6 +58,21 @@ function Main(props) {
         <p className="NotificationTitleText">메인페이지</p>
         <div className="MainBodyDiv">
           <Paper className={classes.root}>
+          <p>공지사항 최신글 10개</p>
+            <Table className={classes.table}>
+              <TableHead>
+                <TableRow>
+                  <TableCell className={classes.tableRow}>분류</TableCell>
+                  <TableCell className={classes.tableRow}>제목</TableCell>
+                  <TableCell className={classes.tableRow}>날짜</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {notifications.map(c => {return (<NotificationInMain key={c.notice_number} id={c.notice_number} category={c.category} title={c.title} date={c.notice_date}/>)})}
+              </TableBody>
+            </Table>
+          </Paper>
+          <Paper className={classes.root}>
           <p>고객센터 최신글 10개</p>
             <Table className={classes.table}>
               <TableHead>
@@ -69,22 +84,7 @@ function Main(props) {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {notifications.map(c => {return (<NotificationInMain key={c.notice_number} id={c.notice_number} category={c.category} title={c.title} date={c.notice_date}/>)})}
-              </TableBody>
-            </Table>
-          </Paper>
-          <Paper className={classes.root}>
-          <p>공지사항 최신글 10개</p>
-            <Table className={classes.table}>
-              <TableHead>
-                <TableRow>
-                  <TableCell className={classes.tableRow}>분류</TableCell>
-                  <TableCell className={classes.tableRow}>제목</TableCell>
-                  <TableCell className={classes.tableRow}>날짜</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {queries.map(c => {return (<QueryInMain key={c.notice_number} id={c.notice_number} category={c.category} title={c.title} date={c.notice_date}/>)})}
+                {queries.map(c => {return (<QueryInMain key={c.notice_number} id={c.notice_number} category={c.category} title={c.title} date={c.board_date} userID={c.user_id}/>)})}
               </TableBody>
             </Table>
           </Paper>
@@ -97,6 +97,18 @@ function Main(props) {
         <p className="NotificationTitleText">메인페이지</p>
         <div className="MainBodyDiv">
           <Paper className={classes.root}>
+          <p>공지사항 최신글 10개</p>
+            <Table className={classes.table}>
+              <TableHead>
+                <TableRow>
+                  <TableCell className={classes.tableRow}>분류</TableCell>
+                  <TableCell className={classes.tableRow}>제목</TableCell>
+                  <TableCell className={classes.tableRow}>날짜</TableCell>
+                </TableRow>
+              </TableHead>
+            </Table>
+          </Paper>
+          <Paper className={classes.root}>
           <p>고객센터 최신글 10개</p>
             <Table className={classes.table}>
               <TableHead>
@@ -104,18 +116,6 @@ function Main(props) {
                   <TableCell className={classes.tableRow}>분류</TableCell>
                   <TableCell className={classes.tableRow}>제목</TableCell>
                   <TableCell className={classes.tableRow}>작성자ID</TableCell>
-                  <TableCell className={classes.tableRow}>날짜</TableCell>
-                </TableRow>
-              </TableHead>
-            </Table>
-          </Paper>
-          <Paper className={classes.root}>
-          <p>공지사항 최신글 10개</p>
-            <Table className={classes.table}>
-              <TableHead>
-                <TableRow>
-                  <TableCell className={classes.tableRow}>분류</TableCell>
-                  <TableCell className={classes.tableRow}>제목</TableCell>
                   <TableCell className={classes.tableRow}>날짜</TableCell>
                 </TableRow>
               </TableHead>
