616dba5b6c8e95d7dff58fb04a928e71506e096d
Jaeuk <enrtja5980@ajou.ac.kr>
2021-12-09 11:16:18 +0900

    상품 삭제 구현

--- PyeonHee/admin/src/pages/PensionBoard.js
+++ PyeonHee/admin/src/pages/PensionBoard.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import '../App.css';
 
-function PensionBoard(props) {
+function PensionBoard({match}) {
   const [productName, setProductName] = useState('');
   const [productCate, setProductCate] = useState('펀드');
+  const [productBankName, setProductBankName] = useState('');
 
   //연금
   const [disconnected, setDisconnected] = useState('');
@@ -12,6 +13,27 @@ function PensionBoard(props) {
   const [interest, setInterest] = useState('');  //연금 공통, 대출 공통
 
   const [link, setLink] = useState('');
+
+  const deleteBoard =()=>{
+    axios({
+        method:"POST",
+        url: `/pensionDelete`,
+        data:{
+            boardID: match.params.boardID,
+        }
+    })
+    .then((res)=>{
+        if(res.data.status === 'success'){
+            alert('삭제 성공');
+            document.location.href = '/financialPensionList/1';
+        }else{
+            alert('삭제 실패');
+        }
+    }).catch(error=>{
+        console.log(error);
+    });
+  }
+
 /*
   useEffect(() => {
     axios({
@@ -25,6 +47,7 @@ function PensionBoard(props) {
         console.log(res.data[0]);
 
         setProductName(res.data[0].productName);
+        setProductBankName(res.data[0].productBankName);
         setProductCate(res.data[0].productCate);
         setDisconnected(res.data[0].disconnected);
         setInterest(res.data[0].interest);
@@ -43,15 +66,19 @@ function PensionBoard(props) {
         <div className="FinancialWriteBodyDiv">
             <div className="BoardWriteTitleDiv">
             <p className="NotificationBoardTitleFont">상품명:&nbsp;</p>
-            <p className="BoardTitle">하하하</p>
+            <p className="FinancialBoardTitle">하하하</p>
+            </div>
+            <div className="BoardWriteTitleDiv">
+                <p className="FinancialBankFont">상품 회사명:&nbsp;</p>
+                <p className="FinancialBankNameTitle">국민은행</p>
             </div>
             <div className="LinkDiv">
             <p className="LinkFont">상품링크:&nbsp;</p>
-            <p className="LinkText">하하하</p>
+            <p className="LinkTextInBoard">www.naver.com</p>
             </div>
             <div className="BoardCateInputDiv">
-                <p className="NotificationBoardCateFont">상품 분류: </p>
-                <p>적금</p>
+                <p className="NotificationBoardCateFont">상품 분류:&nbsp;</p>
+                <p className="FinancialCateInBoard">연금</p>
             </div>
             <div className="FinancialFundDiv">
                 <div className="FinancialFundWriteDiv">
@@ -69,6 +96,9 @@ function PensionBoard(props) {
                           <p>%</p>
                       </div>
                   </div>
+            </div>
+            <div className="NotificationBoardButtonDiv">
+            <button className="NotificationDeleteButton" type='button' onClick={deleteBoard}>삭제</button>
             </div>
         </div>
         </div>
--- PyeonHee/admin/src/pages/SavingBoard.js
+++ PyeonHee/admin/src/pages/SavingBoard.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import '../App.css';
 
-function SavingBoard(props) {
+function SavingBoard({match}) {
   const [productName, setProductName] = useState('');
   const [productCate, setProductCate] = useState('펀드');
+  const [productBankName, setProductBankName] = useState('');
 
   //적금
   const [type, setType] = useState(''); 
@@ -12,6 +13,28 @@ function SavingBoard(props) {
   const [maxInterest, setMaxInterest] = useState('');
 
   const [link, setLink] = useState('');
+
+  const deleteBoard =()=>{
+    axios({
+        method:"POST",
+        url: `/savingDelete`,
+        data:{
+            boardID: match.params.boardID,
+        }
+    })
+    .then((res)=>{
+        if(res.data.status === 'success'){
+            alert('삭제 성공');
+            document.location.href = '/financialSavingList/1';
+        }else{
+            alert('삭제 실패');
+        }
+    }).catch(error=>{
+        console.log(error);
+    });
+  }
+
+
 /*
   useEffect(() => {
     axios({
@@ -25,6 +48,7 @@ function SavingBoard(props) {
         console.log(res.data[0]);
 
         setProductName(res.data[0].productName);
+        setProductBankName(res.data[0].productBankName);
         setProductCate(res.data[0].productCate);
         setType(res.data[0].type);
         setInterest(res.data[0].interest);
@@ -41,17 +65,21 @@ function SavingBoard(props) {
         <p className="NotificationTitleText">상품 확인</p>
         <div className="NotificationWriteDiv">
         <div className="FinancialWriteBodyDiv">
-            <div className="BoardWriteTitleDiv">
+        <div className="BoardWriteTitleDiv">
             <p className="NotificationBoardTitleFont">상품명:&nbsp;</p>
-            <p className="BoardTitle">하하하</p>
+            <p className="FinancialBoardTitle">하하하</p>
+            </div>
+            <div className="BoardWriteTitleDiv">
+                <p className="FinancialBankFont">상품 회사명:&nbsp;</p>
+                <p className="FinancialBankNameTitle">국민은행</p>
             </div>
             <div className="LinkDiv">
             <p className="LinkFont">상품링크:&nbsp;</p>
-            <p className="LinkText">하하하</p>
+            <p className="LinkTextInBoard">www.naver.com</p>
             </div>
             <div className="BoardCateInputDiv">
-                <p className="NotificationBoardCateFont">상품 분류: </p>
-                <p>적금</p>
+                <p className="NotificationBoardCateFont">상품 분류:&nbsp;</p>
+                <p className="FinancialCateInBoard">적금</p>
             </div>
             <div className="FinancialFundDiv">
             <div className="FinancialFundWriteDiv">
@@ -70,6 +98,9 @@ function SavingBoard(props) {
                         <p>%</p>
                       </div>
                     </div>
+            </div>
+            <div className="NotificationBoardButtonDiv">
+            <button className="NotificationDeleteButton" type='button' onClick={deleteBoard}>삭제</button>
             </div>
         </div>
         </div>
