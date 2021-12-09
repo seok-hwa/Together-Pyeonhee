ce04de81a443c30eeb9cb76014d066e06459a540
jyoon22 <yoon22122@gmail.com>
2021-12-09 23:54:34 +0900

    update schedule.js

--- PyeonHee/server/main.js
+++ PyeonHee/server/main.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const mysql = require('mysql');
-var db = require('./db_config.js');
+//const mysql = require('mysql');
+//var db = require('./db_config.js');
+//const admin = require('firebase-admin');
+//let serviceAccount = require('./pyeonhee-AccountKey.json');
 var request = require('request');
-const admin = require('firebase-admin');
-let serviceAccount = require('./pyeonhee-AccountKey.json');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
