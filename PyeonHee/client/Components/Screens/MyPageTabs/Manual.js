f920316842f85b8c6cf69d16fe08438a40bef642
soy <thdl9893@ajou.ac.kr>
2021-12-13 15:33:11 +0900

    사용설명서 추가

--- PyeonHee/client/Components/Screens/MyPageTabs/Manual.js
+++ PyeonHee/client/Components/Screens/MyPageTabs/Manual.js
@@ -9,8 +9,9 @@ import {
     ScrollView,
     Alert
 } from 'react-native';
+import BackButton from '../../Buttons/BackButton';
 
-const ManualScreen = ({navigation, route}) => {
+const Manual = ({navigation, route}) => {
     const [selectedIdx, setSelectedIdx] = useState(0);
 
     const handLeftBotton = () => {
@@ -22,11 +23,8 @@ const ManualScreen = ({navigation, route}) => {
     }
 
     const handRightBotton = () => {
-        if(selectedIdx + 1 === 7){
-            Alert.alert('환영합니다!','편히가계와 스마트한 자산관리를\n 진행하세요.', [
-                {text: '확인', onPress:() => navigation.replace('Main')}
-              ]
-            );
+        if(selectedIdx + 1 > 7){
+            return;
         } else {
             setSelectedIdx(selectedIdx + 1);
         }
@@ -35,11 +33,14 @@ const ManualScreen = ({navigation, route}) => {
     return (
         <View style={styles.appSize}>
             <View style={styles.appTopBar}>
+                    <BackButton onPress={()=>{navigation.goBack()}}/>
                     <View style={{flexDirection: 'row', alignItems: 'flex-end'}}>
                         <Text style={styles.appTopBarText}>편히가계</Text>
                         <Text style={{color: 'black'}}> 란?</Text>
                     </View>
+                    
             </View>
+            
             <View style={styles.rowContainer}>
                 {
                     selectedIdx > 0 ? 
@@ -54,7 +55,7 @@ const ManualScreen = ({navigation, route}) => {
                     selectedIdx === 0 && 
                     <View>
                         <Text style={styles.titleText}>1. 편히-데일리: 일일 내역 확인</Text>
-                        <Image source={require('./assets/manual/pyeonhee_daily.jpg')} style={styles.manualImage}/>
+                        <Image source={require('../assets/manual/pyeonhee_daily.jpg')} style={styles.manualImage}/>
                     </View>
                     
                     
@@ -63,7 +64,7 @@ const ManualScreen = ({navigation, route}) => {
                     selectedIdx === 1 && 
                     <View>
                         <Text style={styles.titleText}>2. 가계부-타인: 타인 예산계획서 열람</Text>
-                        <Image source={require('./assets/manual/budget_others.jpg')} style={styles.manualImage}/>
+                        <Image source={require('../assets/manual/budget_others.jpg')} style={styles.manualImage}/>
                     </View>
                 }
                 {
@@ -71,7 +72,7 @@ const ManualScreen = ({navigation, route}) => {
                     
                     <View>
                         <Text style={styles.titleText}>3. 가계부-본인: 예산계획서 작성 및 수정</Text>
-                        <Image source={require('./assets/manual/budget_mybudget.jpg')} style={styles.manualImage}/>
+                        <Image source={require('../assets/manual/budget_mybudget.jpg')} style={styles.manualImage}/>
                     </View>
                 }
                 {
@@ -79,7 +80,7 @@ const ManualScreen = ({navigation, route}) => {
                     
                     <View>
                         <Text style={styles.titleText}>4. 자산-계좌연동: 계좌연동 및 해지</Text>
-                        <Image source={require('./assets/manual/asset_linking.jpg')} style={styles.manualImage}/>
+                        <Image source={require('../assets/manual/asset_linking.jpg')} style={styles.manualImage}/>
                     </View>
                     
                 }
@@ -87,7 +88,7 @@ const ManualScreen = ({navigation, route}) => {
                     selectedIdx === 4 && 
                     <View>
                         <Text style={styles.titleText}>5. 편히-거래내역: 거래내역 확인</Text>
-                        <Image source={require('./assets/manual/pyeonhee_transaction.jpg')} style={styles.manualImage}/>
+                        <Image source={require('../assets/manual/pyeonhee_transaction.jpg')} style={styles.manualImage}/>
                     </View>
                     
                 }
@@ -95,31 +96,35 @@ const ManualScreen = ({navigation, route}) => {
                     selectedIdx === 5 && 
                     <View>
                         <Text style={styles.titleText}>6. 월별 소비 분석 리포트</Text>
-                        <Image source={require('./assets/manual/monthlyReport.jpg')} style={styles.manualImage}/>
+                        <Image source={require('../assets/manual/monthlyReport.jpg')} style={styles.manualImage}/>
                     </View>
                 }
                 {
                     selectedIdx === 6 && 
                     <View>
                         <Text style={styles.titleText}>7. 자산-금융상품: 금융상품 추천 서비스</Text>
-                        <Image source={require('./assets/manual/asset_financialProduct.jpg')} style={styles.manualImage}/>
+                        <Image source={require('../assets/manual/asset_financialProduct.jpg')} style={styles.manualImage}/>
                     </View>
                 }
                 {
                     selectedIdx === 7 && 
                     <View>
                         <Text style={styles.titleText}>8. 자산-상담사: 자산 상담가 매칭 서비스</Text>
-                        <Image source={require('./assets/manual/asset_counseling.jpg')} style={styles.manualImage}/>
+                        <Image source={require('../assets/manual/asset_counseling.jpg')} style={styles.manualImage}/>
                     </View>
                 }
 
-                <TouchableOpacity
-                    onPress={handRightBotton}
-                >
-                    <Icon name={'chevron-forward-outline'} size={20} color={'#203864'}/>
-                </TouchableOpacity>
+                {
+                    selectedIdx < 7 ?
+                    <TouchableOpacity
+                        onPress={handRightBotton}
+                    >
+                        <Icon name={'chevron-forward-outline'} size={20} color={'#203864'}/>
+                    </TouchableOpacity>
+                    :
+                    <Icon name={'chevron-forward-outline'} size={20} color={'white'}/>
+                }
             </View>
-            
         </View>
     )
 }
@@ -165,4 +170,4 @@ const styles = StyleSheet.create({
     }
 });
 
-export default ManualScreen;
+export default Manual;
